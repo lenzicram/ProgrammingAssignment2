308cd075d192a272cb3f35985e6b400c423a63d8
lenzicram <cramlenzi@gmail.com>
2017-04-17 12:46:13 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This code contains the functions makeCacheMatrix and 
+## cacheSolve. This is an exercise on lexical looping and 
+## invertible matrices
 
-## Write a short comment describing this function
+
+## makeCacheMatrix is a function that creates a special 
+## "matrix" object that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
+        
+         m<-NULL
+  set<-function(y){
+    x<<-y
+    m<<-NULL
+  }
+  get<-function() x
+  setmatrix<-function(solve) m<<- solve
+  getmatrix <- function() m
+  list (set=set, get=get, setmatrix=setmatrix, getmatrix=getmatrix)
 
 }
 
 
-## Write a short comment describing this function
+## cacheSolve is a function that computes the inverse of the 
+## special "matrix" returned by makeCacheMatrix.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x=matrix(), ...) {
+         m <- x$getmatrix()
+  if (!is.null(m)){
+    message("getting cached data")
+    return(m)
+  }
+  matrix <- x$get()
+  m <- solve(matrix, ...)
+  x$setmatrix(m)
+  m
 }
